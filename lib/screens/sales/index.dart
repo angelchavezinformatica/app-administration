7fefba2530c6810db753a36b57067b5081e09e12
angelchavezinformatica <achavezg@unitru.edu.pe>
2024-07-26 12:50:21 -0500

    screen- Sales screen

--- lib/screens/sales/index.dart
+++ lib/screens/sales/index.dart
@@ -1,10 +1,83 @@
+import 'package:app/components/button.dart';
+import 'package:app/constants/color.dart';
+import 'package:app/types/sale.dart';
 import 'package:flutter/material.dart';
 
-class SalesScreen extends StatelessWidget {
+class SalesScreen extends StatefulWidget {
   const SalesScreen({super.key});
 
   @override
+  State<SalesScreen> createState() => _SalesScreenState();
+}
+
+class _SalesScreenState extends State<SalesScreen> {
+  List<Sale> sales = [
+    const Sale(
+        id: 1,
+        date: '26/07/2024',
+        total: 25,
+        customer: 1,
+        customerName: 'Félix Sánchez',
+        details: []),
+    const Sale(
+        id: 1,
+        date: '26/07/2024',
+        total: 25,
+        customer: 1,
+        customerName: 'Dayanara Costa',
+        details: [])
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Text('Ventas');
+    return Column(children: [
+      primaryButton(() {}, 'Registrar Venta'),
+      Expanded(
+          child: ListView.builder(
+              itemCount: sales.length,
+              itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                  child: saleW(sales[index]),
+                );
+              })),
+    ]);
+  }
+
+  Widget saleW(Sale sale) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      decoration: BoxDecoration(
+          border: Border.all(width: 2),
+          borderRadius: BorderRadius.circular(10)),
+      child: Column(
+        children: [
+          Text(sale.customerName,
+              style: const TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: greenColor)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Fecha: ${sale.date}',
+                style: const TextStyle(fontSize: 16),
+              ),
+              const SizedBox(width: 20),
+              Text(
+                'Total: S/ ${sale.total.toString()}',
+                style: const TextStyle(fontSize: 16),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
---
+++ lib/types/sale.dart
@@ -0,0 +1,34 @@
+class Sale {
+  final int? id;
+  final String date;
+  final double total;
+  final int customer;
+  final String customerName;
+  final List<SaleDetail> details;
+
+  const Sale(
+      {this.id,
+      required this.date,
+      required this.total,
+      required this.customer,
+      required this.customerName,
+      required this.details});
+}
+
+class SaleDetail {
+  final int? id;
+  final double price;
+  final double cant;
+  final double subtotal;
+  final int idSale;
+  final int idProduct;
+
+  const SaleDetail({
+    this.id,
+    required this.price,
+    required this.cant,
+    required this.subtotal,
+    required this.idSale,
+    required this.idProduct,
+  });
+}
