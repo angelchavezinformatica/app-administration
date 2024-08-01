9c5b436976f042373f9f90b11f53b29200296909
angelchavezinformatica <achavezg@unitru.edu.pe>
2024-07-31 19:40:41 -0500

    lexer-add- test sql lexer

--- sql_lexer.py
+++ sql_lexer.py
@@ -2,15 +2,14 @@ import ply.lex as lex
 
 
 class SQLLexer:
-    # Lista de tokens
     tokens = (
         'SELECT', 'INSERT', 'UPDATE', 'DELETE', 'CREATE',
         'TABLE', 'FROM', 'INTO', 'VALUES', 'SET', 'WHERE',
         'COMMA', 'SEMICOLON', 'LPAREN', 'RPAREN', 'EQUAL',
-        'STAR', 'LESS', 'GREATER', 'IDENTIFIER', 'NUMBER', 'STRING'
+        'STAR', 'LESS', 'GREATER', 'IDENTIFIER', 'NUMBER', 'STRING',
+        'PLUS', 'MINUS', 'DIVIDE', 'TIMES', 'DOT'
     )
 
-    # Reglas de expresiones regulares para tokens simples
     t_SELECT = r'SELECT'
     t_INSERT = r'INSERT'
     t_UPDATE = r'UPDATE'
@@ -30,40 +29,38 @@ class SQLLexer:
     t_STAR = r'\*'
     t_LESS = r'<'
     t_GREATER = r'>'
+    t_PLUS = r'\+'
+    t_MINUS = r'-'
+    t_DIVIDE = r'/'
+    t_TIMES = r'\*'
+    t_DOT = r'\.'
     t_ignore = ' \t'
 
-    # Expresión regular para identificadores
     def t_IDENTIFIER(self, t):
         r'[a-zA-Z_][a-zA-Z_0-9]*'
         return t
 
-    # Expresión regular para numeros
     def t_NUMBER(self, t):
-        r'\d+'
-        t.value = int(t.value)
+        r'-?\d+(\.\d+)?'
+        t.value = float(t.value)
         return t
 
-    # Expresión regular para comillas
     def t_STRING(self, t):
         r'\'[^\']*\''
-        t.value = t.value[1:-1]  # Quita las comillas
+        t.value = t.value[1:-1]
         return t
 
-    # Manejo de nuevas lineas
     def t_newline(self, t):
         r'\n+'
         t.lexer.lineno += len(t.value)
 
-    # Manejo de errores
     def t_error(self, t):
         print(f"Illegal character '{t.value[0]}'")
         t.lexer.skip(1)
 
-    # Crear el lexer
     def build(self, **kwargs):
         self.lexer = lex.lex(module=self, **kwargs)
 
-    # Ejecutar el lexer
     def test(self, data: str):
         self.lexer.input(data)
         while True:
@@ -77,13 +74,24 @@ class SQLLexer:
 
 
 if __name__ == '__main__':
+    import sys
+
+    is_verbose = False
+
+    if len(sys.argv) > 1 and sys.argv[1] in ('-v', '--verbose'):
+        is_verbose = True
+
     from lexer.sql_lexer_query import QUERYS
 
     lexer = SQLLexer()
     lexer.build()
 
     for query in QUERYS:
-        print(query)
+        if is_verbose:
+            print(query)
         for token in lexer.test(query):
-            print(token)
-        print('-'*20)
+            if is_verbose:
+                print(token)
+            pass
+        if is_verbose:
+            print('-'*20)
